76a14a053ed22d7bd11b2b4555eda91a86e48d58
Tieu Phong <tieuphong@cxptek.com>
2024-01-24 18:22:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useRef, useState } from "react";
+
 import "./App.css";
 interface ABIFunction {
   name: string;
